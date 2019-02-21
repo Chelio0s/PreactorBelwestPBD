2418ef52d4852074f98ee1f8aa497b78499f0a75
kovalkou <zima12343@mail.ru>
2019-02-21 14:17:57 +0300

    Update

--- PreactorPBD/Tables/Nomenclature.sql
+++ PreactorPBD/Tables/Nomenclature.sql
@@ -2,8 +2,8 @@
 (
 	[IdNomenclature] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ArticleId] INT NOT NULL, 
-    [Number_] NVARCHAR(50) NOT NULL UNIQUE, 
-    [Size] INT NOT NULL, 
+    [Number_] NVARCHAR(99) NOT NULL UNIQUE, 
+    [Size] FLOAT NOT NULL, 
     [CountPersent] DECIMAL(6, 2) NOT NULL, 
     CONSTRAINT [FK_Nomenclature_ToArticle] FOREIGN KEY (ArticleID) REFERENCES [Article]([IdArticle])
 
