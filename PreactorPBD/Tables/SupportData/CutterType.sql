98c5b2b3cb9c67270987bbfc8a6f3f207515fa81
chelio0s <se.chelios@gmail.com>
2020-01-21 14:16:06 +0300

    таблички под резаки

---
+++ PreactorPBD/Tables/SupportData/CutterType.sql
@@ -0,0 +1,6 @@
+﻿--types of cutters
+CREATE TABLE [SupportData].[CutterType]
+(
+	[IdCutterType]	INT				NOT NULL PRIMARY KEY IDENTITY, 
+    [Tytle]			NVARCHAR(99)	NOT NULL
+)
---
+++ PreactorPBD/Tables/SupportData/CuttersForKTOPs.sql
@@ -0,0 +1,9 @@
+﻿--used cutters for operations
+CREATE TABLE [SupportData].[CuttersForKTOPs]
+(
+	[IdCutterForKTOP]		INT				NOT NULL PRIMARY KEY IDENTITY, 
+    [KTOP]					INT				NOT NULL, 
+    [TypeCutterId]			INT				NOT NULL, 
+    [UseCount]				DECIMAL(4, 2)	NOT NULL
+	CONSTRAINT FK_Cutters_Types FOREIGN KEY([TypeCutterId]) REFERENCES [SupportData].[CutterType](IdCutterType)
+)
