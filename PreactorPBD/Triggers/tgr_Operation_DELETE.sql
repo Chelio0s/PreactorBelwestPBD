bfce47f334fc4ef550d3b6dfa80d6c268f61a046
zima12343 <zima12343@mail.ru>
2020-05-12 11:45:26 +0300

    Добавил триггер на удаление операций

---
+++ PreactorPBD/Triggers/tgr_Operation_DELETE.sql
@@ -0,0 +1,7 @@
+﻿--11.05.2020 Kovalkov
+CREATE TRIGGER [tgr_Operation_DELETE]
+ON [InputData].[Operations]
+AFTER DELETE
+AS
+DELETE FROM [InputData].[OperationWithKTOP] 
+WHERE OperationId IN (SELECT [IdOperation] FROM DELETED)
