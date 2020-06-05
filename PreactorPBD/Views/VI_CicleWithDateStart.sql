4d592c32a151ea54058cd326ea5138bb80971b6b
Sergey Trofimov <se.chelios@gmail.com>
2020-06-05 10:06:30 +0300

    Добавил цех

--- PreactorPBD/Views/VI_CicleWithDateStart.sql
+++ PreactorPBD/Views/VI_CicleWithDateStart.sql
@@ -2,11 +2,13 @@
 	AS SELECT TOP 100 PERCENT [IdCicleUseFrom]
       ,[CicleId]
       ,[StartUseFrom]
-	  ,cc.DurationWork
-	  ,cc.DurationOff
+	  ,cc.[DurationWork]
+	  ,cc.[DurationOff]
       ,[SpecificOrgUnit]
-	  ,cc.ShiftId
-	  ,cc.AreaId
-  FROM [SupportData].[CicleUseFrom]	AS cuf 
-  INNER JOIN [SupportData].[Cicle]					AS cc  ON cc.IdCicle = cuf.CicleId
+	  ,cc.[ShiftId]
+	  ,cc.[AreaId]
+      ,area.[Title]
+  FROM [SupportData].[CicleUseFrom]      AS cuf 
+  INNER JOIN [SupportData].[Cicle]		 AS cc      ON cc.IdCicle = cuf.CicleId
+  INNER JOIN [InputData].[Areas]         AS area    ON area.IdArea = cc.AreaId        
   ORDER BY [StartUseFrom] DESC, cc.AreaId, ShiftId, SpecificOrgUnit, CicleId
